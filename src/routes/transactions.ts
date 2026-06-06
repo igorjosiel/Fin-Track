39c4d04a2bc77791deec740c2e50fd1f40698f4e
igorjosiel <igorjosielvitalino@gmail.com>
2026-06-06 09:33:31 -0300

    feat: adiciona cookies na rota de post e cria middleware para verificar a existência desse cookie.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,37 +2,61 @@ import type { FastifyInstance } from "fastify";
 import { z } from "zod";
 import crypto from "node:crypto";
 import { db as knex } from "../database.js";
-import { type ITransaction } from "../@types/knex.js";
+import { type ITransaction } from "../@types/knex.d.js";
+import { checkSessionIdExists } from "../../middlewares/check-session-id-exists.js";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get("/", async (request, reply) => {
-        const transactions = await knex("transactions")
-            .select();
+    app.get(
+        "/",
+        { preHandler: [checkSessionIdExists] },
+        async (request, reply) => {
+            const { sessionId } = request.cookies;
 
-        return { transactions }
-    });
+            const transactions = await knex("transactions")
+                .where("session_id", sessionId)
+                .select();
 
-    app.get("/:id", async (request, reply) => {
-        const getTransactionParamsSchema = z.object({
-            id: z.uuid(),
-        });
+            return { transactions }
+        }
+    );
 
-        const { id } = getTransactionParamsSchema.parse(request.params);
+    app.get(
+        "/:id",
+        { preHandler: [checkSessionIdExists] },
+        async (request, reply) => {
+            const { sessionId } = request.cookies;
 
-        const transaction = await knex<ITransaction>("transactions")
-            .where("id", id)
-            .first();
+            const getTransactionParamsSchema = z.object({
+                id: z.uuid(),
+            });
 
-        return { transaction }
-    });
+            const { id } = getTransactionParamsSchema.parse(request.params);
 
-    app.get("/summary", async (request, reply) => {
-        const summary = await knex<ITransaction>("transactions")
-            .sum("amount", { as: "amount" })
-            .first();
+            const transaction = await knex<ITransaction>("transactions")
+                .where({
+                    session_id: sessionId,
+                    id,
+                })
+                .first();
 
-        return { summary }
-    });
+            return { transaction }
+        }
+    );
+
+    app.get(
+        "/summary",
+        { preHandler: [checkSessionIdExists] },
+        async (request, reply) => {
+            const { sessionId } = request.cookies;
+
+            const summary = await knex<ITransaction>("transactions")
+                .where("session_id", sessionId)
+                .sum("amount", { as: "amount" })
+                .first();
+
+            return { summary }
+        }
+    );
 
     app.post("/", async (request, reply) => {
         const createTransactionBodySchema = z.object({
@@ -43,10 +67,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const { title, amount, type } = createTransactionBodySchema.parse(request.body);
 
+        let sessionId = request.cookies.sessionId;
+
+        if (!sessionId) {
+            sessionId = crypto.randomUUID();
+
+            reply.cookie("sessionId", sessionId, {
+                path: "/",
+                maxAge: 60 * 60 * 24 * 7 // 7 days
+            });
+        }
+
         await knex<ITransaction>("transactions").insert({
             id: crypto.randomUUID(),
             title,
             amount: type === "credit" ? amount : amount * -1,
+            session_id: sessionId
         });
 
         return reply.status(201).send();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
+
 import { env } from "./env/index.js";
 import { transactionsRoutes } from "./routes/transactions.js";
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transactionsRoutes, {
     prefix: "transactions",
 });
