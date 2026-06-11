a8a5feabfdfb6826d5707a4fc646eb59247e200f
igorjosiel <igorjosielvitalino@gmail.com>
2026-06-11 10:58:15 -0300

    feat: cria um novo teste para listar as transações que possuem determinado cookie e adiciona um novo banco de dados exclusivo para executar os testes.

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,5 +1,6 @@
-import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { describe, it, expect, beforeAll, afterAll, beforeEach } from "vitest";
 import request from "supertest";
+import { execSync } from "node:child_process";
 import { app } from "../src/app.js";
 
 describe("Transactions routes", () => {
@@ -11,8 +12,12 @@ describe("Transactions routes", () => {
         await app.close();
     });
 
+    beforeEach(() => {
+        execSync("npm run knex migrate:rollback --all");
+        execSync("npm run knex migrate:latest");
+    });
+
     it("should be able to create a new transaction", async () => {
-        console.log("Teste: ", app.server);
         const response = await request(app.server).post("/transactions").send({
             title: "New Transaction",
             amount: 5000,
@@ -20,5 +25,27 @@ describe("Transactions routes", () => {
         });
 
         expect(response.statusCode).toEqual(201);
+    });
+
+    it("should be able to list all the transactions", async () => {
+        const createTransactionResponse = await request(app.server).post("/transactions").send({
+            title: "New Transaction",
+            amount: 5000,
+            type: "credit"
+        });
+
+        const cookies = createTransactionResponse.get("Set-Cookie");
+
+        const listTransactionsResponse = await request(app.server)
+            .get("/transactions")
+            .set("Cookie", cookies)
+            .expect(200);
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: "New Transaction",
+                amount: 5000,
+            }),
+        ]);
     });
 });
